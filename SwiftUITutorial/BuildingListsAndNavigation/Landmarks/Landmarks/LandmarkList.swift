f5524b64598042513447f0fb01353e7d20f55419
ChaunceyKiwi <chaunceykiwi@gmail.com>
2020-09-12 21:59:22 -0700

    Add list and navigation tutorial

---
+++ SwiftUITutorial/BuildingListsAndNavigation/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  List.swift
+//  Landmarks
+//
+//  Created by Chauncey on 2020-09-12.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
